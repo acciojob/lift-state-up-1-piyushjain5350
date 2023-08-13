a22003700adabe4ff6f6e1313da3bc952182c01a
Piyush Jain <piyushjain5350@gmail.com>
2023-08-13 19:05:44 +0530

    1. ans commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,57 @@
+// import React from "react";
+// import './../styles/App.css';
+// import Parent from "./Parent";
 
-import React from "react";
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//         <Parent/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <ParentComponent />
     </div>
   )
 }
 
-export default App
+const ParentComponent = () => {
+
+  let [showModal, setShowModal] = useState(false)
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <ChildComponent showModal={showModal} setShowModal={setShowModal} />
+    </div>
+  )
+}
+
+const ChildComponent = ( {showModal, setShowModal}) => {
+
+  const handleShowModal = () => {
+    setShowModal(true)
+  }
+  return (
+    <div>
+      <h1>Child Component</h1>
+      <button onClick={handleShowModal}>Show Modal</button>
+      {showModal ? (
+        <div>
+          <h3>Modal Content</h3>
+          <p>This is the modal content.</p>
+        </div>
+      ): null}
+    </div>
+  )
+}
+     
+export default App;
